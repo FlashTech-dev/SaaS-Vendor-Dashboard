2054457516d9388ef4a202d4dabea42f418e5f62
Karan <karanwagh17@gmail.com>
2021-01-16 14:54:31 +0530

    SaaS dashboard

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from '../components/Dashboard';
+import './App.css';
+function App() {
+  return (
+    <>
+      <Dashboard/>
+      Lets go dashboard
+    </>
+  );
+}
+
+export default App;
